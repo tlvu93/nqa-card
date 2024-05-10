bea8bfbbabc998b2e2c5e67054746b7d8d8c5ea9
Tuan <tv@vergelink.io>
2024-05-10 15:15:29 +0200

    Add Login Page

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -1,23 +1,20 @@
-import { Title, Text, Anchor } from '@mantine/core';
+import { Title, Text, Container } from '@mantine/core';
 import classes from './Welcome.module.css';
 
 export function Welcome() {
   return (
-    <>
-      <Title className={classes.title} ta="center" mt={100}>
+    <Container>
+      <Title className={classes.title} ta="center">
         Welcome to{' '}
-        <Text inherit variant="gradient" component="span" gradient={{ from: 'pink', to: 'yellow' }}>
-          Mantine
+        <Text inherit variant="gradient" component="span" gradient={{ from: 'blue', to: 'violet' }}>
+          No Question Asked
         </Text>
       </Title>
       <Text color="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
-        This starter Next.js project includes a minimal setup for server side rendering, if you want
-        to learn more on Mantine + Next.js integration follow{' '}
-        <Anchor href="https://mantine.dev/guides/next/" size="lg">
-          this guide
-        </Anchor>
-        . To get started edit index.tsx file.
+        &ldquo;Hey there! Welcome to &lsquo;No Question Asked&lsquo;—where sealing an unbreakable
+        promise with your friends is just a few clicks away. Ever want to ensure a buddy commits to
+        task without asking any questions? Create a voucher that makes it official.&rdquo;
       </Text>
-    </>
+    </Container>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,34 @@
+import { Center, Grid, em } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+
+import { AuthenticationForm } from '@/components/AuthenticationForm';
 
 export default function HomePage() {
+  const isMobile = useMediaQuery(`(max-width: ${em(1024)})`);
+
+  const centerStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minHeight: '100vh',
+    padding: em(32),
+    overflow: 'hidden',
+  };
+
   return (
-    <>
-      <Welcome />
-      <ColorSchemeToggle />
-    </>
+    <div style={centerStyle}>
+      <Grid h="100%">
+        <Grid.Col hidden={isMobile} span={6} h="100%">
+          <Welcome />
+        </Grid.Col>
+
+        <Grid.Col span={isMobile ? 12 : 6}>
+          <Center h="100%">
+            <AuthenticationForm />
+          </Center>
+        </Grid.Col>
+      </Grid>
+    </div>
   );
 }
